37287692cd0149f6a1dff95fbbc3ebb8ae27c000
RasonYudha4 <rasonyudha921@gmail.com>
2026-04-06 08:59:05 +0700

    backend folder structure

--- frontend/src/layouts/MasterAdminLayout.tsx
+++ frontend/src/layouts/MasterAdminLayout.tsx
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router";
+
 export default function MasterAdminLayout() {
     return (
         <div>
             Master Admin Layout
+            <Outlet />
         </div>
     )
 }
